5d387a9e0c57cca329fef6ab9cebb11611e31577
Thomas <43654548+thethomaswgeorge@users.noreply.github.com>
2020-03-25 22:41:50 -0500

    Finished Assign2 1

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ellipticCurve import ellipticCurveSolver
 from primeChecker import primeChecker
 from new_simple import *
+from simply_exp import *
 
 
 def show_menu():
@@ -11,19 +12,10 @@ def show_menu():
     print("\"elliptic\": to calculate an elliptic curve of style Y=y^2 = x^3 + ax + b (mod p)")
     print("\"prime check\": check if an extremely large integer is prime")
     print("\"new_simple\": problem like 13x+5= 15(mod 23)")
+    print("\"simplify\": 'Simplify the following expressions")
     print("\"menu\": to see all available options")
     print("\"ecadd\"")
     print("\"end\": to close program\n")
-
-
-# greatest common divisor
-def gcd(a, n, b):
-    x = 0
-    for x in range(1, n):
-        if a * x % n == b:
-            print("gcd = ", x)
-            return x
-
 
 def main():
     print("welcome to the Modulus Calculator")
@@ -34,7 +26,12 @@ def main():
     print("type \"menu\" to see all available options")
     while option != "end":
         option = input("\nWhat would you like to do?: ")
-
+        if option == "simplify":        
+            print("a^b (mod p)")
+            a = int(input("a = "))
+            b = int(input("b = "))
+            p = int(input("p = "))
+            simplify_base(a,b,p)
         if option == "simple":
             print("ax = b mod(n)")
             num = int(input("a = "))
